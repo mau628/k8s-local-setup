314394177ba6ea65893f3c28b42ac42aa071092d
Jorge Gonzalez <39781882+mau628@users.noreply.github.com>
2023-04-25 12:11:43 -0600

    Update master-init.sh

--- master-init.sh
+++ master-init.sh
@@ -11,10 +11,14 @@ printt "Init Master Node"
 kubeadm init
 
 ########################
-printt "Setup kube config"
+printt "Apply kube config"
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
+
+########################
+printt "Apply Calico config"
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
 ########################
 printt "Master Node Init Complete!"
